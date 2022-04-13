bc2b486cdce48fdb6dd571222336ae73c94e2288
isaboyraz <isaboyraz46@gmail.com>
2022-04-13 04:24:23 +0200

    Adding Template to App

--- members/views.py
+++ members/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.template import loader
+from .models import Members
 
 def index(request):
-    return HttpResponse("Hello world!")
+  mymembers = Members.objects.all().values()
+  template = loader.get_template('index.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
